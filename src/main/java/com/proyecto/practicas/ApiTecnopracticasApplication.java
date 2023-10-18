79dec2c430037ebcc1cfd6cdef68a892b949a293
mateo <mateo204r@gmail.com>
2023-10-18 18:36:33 -0500

    proyecto base

---
+++ src/main/java/com/proyecto/practicas/ApiTecnopracticasApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.practicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTecnopracticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTecnopracticasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/practicas/ApiTecnopracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTecnopracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

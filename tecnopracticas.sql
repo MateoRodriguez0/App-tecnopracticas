953cae7771f3e98fca135c63f55696e80f025c5d
mateo <mateo204r@gmail.com>
2023-11-02 07:26:58 -0500

    Relaciones creadas en mysql

--- tecnopracticas.sql
+++ tecnopracticas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 19-10-2023 a las 00:32:36
+-- Tiempo de generación: 02-11-2023 a las 12:24:57
 -- Versión del servidor: 8.0.31
 -- Versión de PHP: 8.0.26
 
@@ -24,64 +24,102 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `admin_practicas`
+-- Estructura de tabla para la tabla `admins_practicas`
 --
 
-DROP TABLE IF EXISTS `admin_practicas`;
-CREATE TABLE IF NOT EXISTS `admin_practicas` (
+DROP TABLE IF EXISTS `admins_practicas`;
+CREATE TABLE IF NOT EXISTS `admins_practicas` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `nombre_completo` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
-  `sexo` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
-  `telefono` varchar(10) COLLATE latin1_spanish_ci NOT NULL,
+  `nombre_completo` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `sexo` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `telefono` varchar(10) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
   `fecha_nacimiento` date NOT NULL,
-  `email` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `carrera`
+-- Estructura de tabla para la tabla `carreras`
 --
 
-DROP TABLE IF EXISTS `carrera`;
-CREATE TABLE IF NOT EXISTS `carrera` (
+DROP TABLE IF EXISTS `carreras`;
+CREATE TABLE IF NOT EXISTS `carreras` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `Nombre_carrera` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `Nombre_carrera` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `empresa`
+-- Estructura de tabla para la tabla `empresas`
 --
 
-DROP TABLE IF EXISTS `empresa`;
-CREATE TABLE IF NOT EXISTS `empresa` (
+DROP TABLE IF EXISTS `empresas`;
+CREATE TABLE IF NOT EXISTS `empresas` (
   `NIT` int NOT NULL AUTO_INCREMENT,
-  `nombre_completo` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
-  `telefono` varchar(10) COLLATE latin1_spanish_ci NOT NULL,
-  `direccion` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
-  `email` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `nombre_completo` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `telefono` varchar(10) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `direccion` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
   PRIMARY KEY (`NIT`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `estudiante`
+-- Estructura de tabla para la tabla `estudiantes`
 --
 
-DROP TABLE IF EXISTS `estudiante`;
-CREATE TABLE IF NOT EXISTS `estudiante` (
+DROP TABLE IF EXISTS `estudiantes`;
+CREATE TABLE IF NOT EXISTS `estudiantes` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `nombre_completo` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
-  `sexo` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
-  `telefono` varchar(10) COLLATE latin1_spanish_ci NOT NULL,
+  `nombre_completo` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `sexo` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `telefono` varchar(10) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
   `fecha_nacimiento` date NOT NULL,
-  `email` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
   PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ofertas_practicas`
+--
+
+DROP TABLE IF EXISTS `ofertas_practicas`;
+CREATE TABLE IF NOT EXISTS `ofertas_practicas` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `nombre_oferta` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `descripcion` text COLLATE latin1_spanish_ci NOT NULL,
+  `idAdministrador` int DEFAULT NULL,
+  `idEmpresa` int NOT NULL,
+  `IdCarrera` int NOT NULL,
+  `fecha_publicacion` datetime NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_idAdministrador` (`idAdministrador`),
+  KEY `fk_idEmpresa` (`idEmpresa`),
+  KEY `fk_idCarrera` (`IdCarrera`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `postulaciones`
+--
+
+DROP TABLE IF EXISTS `postulaciones`;
+CREATE TABLE IF NOT EXISTS `postulaciones` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `idEstudiante` int NOT NULL,
+  `idOferta` int NOT NULL,
+  `fecha_postulacion` datetime NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_idEstudiante` (`idEstudiante`),
+  KEY `fk_idOferta` (`idOferta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 -- --------------------------------------------------------
@@ -93,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `estudiante` (
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE IF NOT EXISTS `roles` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `nombre` varchar(50) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
@@ -104,6 +142,25 @@ CREATE TABLE IF NOT EXISTS `roles` (
 INSERT INTO `roles` (`ID`, `nombre`) VALUES
 (1, 'Estudiante'),
 (2, 'Administrador');
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ofertas_practicas`
+--
+ALTER TABLE `ofertas_practicas`
+  ADD CONSTRAINT `fk_idAdministrador` FOREIGN KEY (`idAdministrador`) REFERENCES `admins_practicas` (`ID`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idCarrera` FOREIGN KEY (`IdCarrera`) REFERENCES `carreras` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idEmpresa` FOREIGN KEY (`idEmpresa`) REFERENCES `empresas` (`NIT`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `postulaciones`
+--
+ALTER TABLE `postulaciones`
+  ADD CONSTRAINT `fk_idEstudiante` FOREIGN KEY (`idEstudiante`) REFERENCES `estudiantes` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idOferta` FOREIGN KEY (`idOferta`) REFERENCES `ofertas_practicas` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

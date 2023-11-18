dca1d5ea9e65159e5046b389596a962eff57602c
Jeyson Miranda <jeyson.miranda11@hotmail.com>
2023-11-18 08:25:40 -0500

    carreraservice

--- src/main/java/com/proyecto/practicas/repositories/UserRepository.java
+++ src/main/java/com/proyecto/practicas/repositories/UserRepository.java
@@ -1,7 +1,15 @@
 package com.proyecto.practicas.repositories;
 
 import com.proyecto.practicas.models.Usuario;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 public interface UserRepository extends JpaRepository<Usuario,Long>{
+
+    Boolean ExistByEmail(String email);
+
 }
--- src/main/java/com/proyecto/practicas/services/UserService.java
+++ src/main/java/com/proyecto/practicas/services/UserService.java
@@ -1,6 +1,22 @@
 package com.proyecto.practicas.services;
 
 
+import com.proyecto.practicas.models.Usuario;
+import com.proyecto.practicas.repositories.UserRepository;
+
+import java.util.List;
+
 public interface UserService {
+
+
+
+    Usuario updateUser(Long userId, Usuario updatedUser);
+
+     void deleteUser(Long userId);
+
+     List<Usuario> getAllUsuarios();
+
+     Boolean ExistUserByEmail(String email);
+
 
 }

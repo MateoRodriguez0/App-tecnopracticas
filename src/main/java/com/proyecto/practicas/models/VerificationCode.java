8e476507f50f67eb8b8ed7c0dcfccd31be822e88
mateo <mateo204r@gmail.com>
2023-11-20 14:13:45 -0500

    configuracion inicial cliente feign

---
+++ src/main/java/com/proyecto/practicas/models/VerificationCode.java
@@ -0,0 +1,18 @@
+package com.proyecto.practicas.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class VerificationCode {
+	
+	private String email;
+	private Integer codigo;
+
+	
+}

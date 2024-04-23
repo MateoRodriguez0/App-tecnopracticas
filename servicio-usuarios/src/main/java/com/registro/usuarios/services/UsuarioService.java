dd0bdada02e3d6fc5b6bb90a6c6d08dce519f4c1
Jeyson Miranda <jeis11@hotmail.com>
2024-04-22 19:22:52 -0500

    add

--- servicio-usuarios/src/main/java/com/registro/usuarios/services/UsuarioService.java
+++ servicio-usuarios/src/main/java/com/registro/usuarios/services/UsuarioService.java
@@ -3,9 +3,12 @@ package com.registro.usuarios.services;
 import com.registro.usuarios.exception.EmailExistException;
 import com.registro.usuarios.models.Usuario;
 import com.registro.usuarios.repositories.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UsuarioService {
@@ -26,5 +29,25 @@ public class UsuarioService {
         usuario.setNombre(nombre);
 
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional
+    public boolean validatePasswordResetToken(String token) {
+        Usuario usuario = usuarioRepository.findByPasswordResetToken(token);
+        if (usuario != null) {
+            LocalDateTime expiration = usuario.getPasswordResetTokenExpiration();
+            LocalDateTime now = LocalDateTime.now();
+            return expiration != null && expiration.isAfter(now);
+        }
+        return false;
+    }
+    @Transactional
+    public void updatePassword(String token, String newPassword) {
+        Usuario usuario = usuarioRepository.findByPasswordResetToken(token);
+        if (usuario != null) {
+            usuario.setPassword(passwordEncoder.encode(newPassword));
+            usuario.setPasswordResetToken(null);
+            usuarioRepository.save(usuario);
+        }
     }
 }

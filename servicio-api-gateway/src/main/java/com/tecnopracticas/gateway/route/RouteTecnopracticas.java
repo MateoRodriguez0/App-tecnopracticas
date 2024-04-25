91c919b6018d93a62c8cac1082b8b1c6439985c0
mateo Rodríguez <mateo204r@gmail.com>
2024-04-25 18:09:46 -0500

    enrutando servicio-usuarios en api gateway

--- servicio-api-gateway/src/main/java/com/tecnopracticas/gateway/route/RouteTecnopracticas.java
+++ servicio-api-gateway/src/main/java/com/tecnopracticas/gateway/route/RouteTecnopracticas.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class RouteTecnopracticas {
@@ -12,6 +13,19 @@ public class RouteTecnopracticas {
 	RouteLocator routesTecnopracticas(RouteLocatorBuilder builder) {
 	
 		return builder.routes()
+				.route("servicio-usuarios",p ->
+				p.path("/usuarios/**")
+				.and()
+				.predicate(s ->{
+					return !s.getRequest().getURI()
+							.getPath().toString().equals("/usuarios/buscar");
+				})
+				.and().method(HttpMethod.GET,HttpMethod.POST)
+				.filters(f ->f.stripPrefix(1) )
+				.uri("http://localhost:10451"))
 				.build();
 	}
+	
+
+	
 }

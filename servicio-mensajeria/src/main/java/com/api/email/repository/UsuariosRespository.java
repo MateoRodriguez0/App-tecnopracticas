523786b2bb761bf90f3784c445b80dc44e64f874
mateo Rodríguez <mateo204r@gmail.com>
2024-05-19 06:06:37 -0500

    save UsuariosRepository

--- servicio-mensajeria/src/main/java/com/api/email/repository/UsuariosRespository.java
+++ servicio-mensajeria/src/main/java/com/api/email/repository/UsuariosRespository.java
@@ -12,4 +12,5 @@ public interface UsuariosRespository extends JpaRepository<Usuario,UUID>{
 	@Query(value = "select u.nombre_completo from usuarios u where u.correo_electronico=?",
 			nativeQuery = true)
 	String getnombreByEmail(String email);
+	
 }
